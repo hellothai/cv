c8ef3605e5e0d814f1b608e50ff2049ebb459779
hellothai <thaismenezes.f@gmail.com>
2021-10-10 16:04:22 +0100

    Update academic

--- src/components/academic/Academic.jsx
+++ src/components/academic/Academic.jsx
@@ -4,33 +4,33 @@ import "./academic.scss"
 export class Academic extends Component {
     render() {
         return (
-            <div className="testimonials" id="testimonials">
+            <div className="academic" id="testimonials">
                 <h2>Académico</h2>
-                Cursando o último semestre de Engenharia de Informática no Instituto Superior de Engenheria do Porto.
+                <h3>Cursando o último semestre de Engenharia de Informática no Instituto Superior de Engenheria do Porto.</h3>
                 <h2>Tecnologias de Desenvolvimento</h2>
-                <ul>
-                    <li>
-                        React
-                    </li>
-                    <li>
-                        TypeScript
-                    </li>
-                    <li>
-                        CSS
-                    </li>
-                    <li>
-                        Base de Dados Relacionais e não relacionais
-                    </li>
-                    <li>
-                        Java
-                    </li>
-                    <li>
-                        Material-UI
-                    </li>
-                    <li>
-                        JavaScript
-                    </li>
-                </ul>
+
+                <li>
+                    React
+                </li>
+                <li>
+                    TypeScript
+                </li>
+                <li>
+                    CSS
+                </li>
+                <li>
+                    Base de Dados Relacionais e não relacionais
+                </li>
+                <li>
+                    Java
+                </li>
+                <li>
+                    Material-UI
+                </li>
+                <li>
+                    JavaScript
+                </li>
+
                 <h2>Tecnologias de Equipa</h2>
                 <li>
                     Jira
